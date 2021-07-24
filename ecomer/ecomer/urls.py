8644aff2a82ed5327dc2608d458b8f2ba08b86fc
Manas <manasdeshpande4902@gmail.com>
2021-07-24 22:23:29 +0530

    try to host

--- ecomer/ecomer/urls.py
+++ ecomer/ecomer/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,  include
 from . import view
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view.hhom,name="hhom"),
     path('d/', view.index,name="index"),
     path("job/", include('job.urls')),
-    path("blog/", include('blog.urls'))
-
-
+    path("blog/", include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 
 ]
